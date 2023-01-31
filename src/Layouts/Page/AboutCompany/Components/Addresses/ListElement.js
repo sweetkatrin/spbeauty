cf2bd981a8c78db55dd0ca138ca59a224fbd5378
SweetKatrin <kate.yagu@gmail.com>
2023-01-31 11:50:29 +0300

    fix secondary button

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -16,8 +16,8 @@ function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
+            <SecondaryButton>Ред.</SecondaryButton>
+            <SecondaryButton>Удл.</SecondaryButton>
         </div>
     );
 }
