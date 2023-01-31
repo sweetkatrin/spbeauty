57d68d821db5f808b0c1eb3337b6bd96aad6c8ae
SweetKatrin <kate.yagu@gmail.com>
2023-01-31 14:09:56 +0300

    change secondary button to icon only button and update style

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,26 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+import styled from "styled-components";
+
+const Container = styled.div`
+    border: 1px solid rgba(100, 116, 139, 0.12);
+    border-radius: 8px;
+    max-width: 45%;
+`;
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
+        <Container className='d-inline-flex flex-column mt-4 p-4 justify-content-between col'>          
+            <div className='d-flex flex-column gap-4'>
+                <div className='title'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-2'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
             </div>
-        </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
+        </Container>
     );
 }
 
