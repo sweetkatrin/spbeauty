2f7d6c5c9fffeb70482aaf98fa339b55567c8fa4
SweetKatrin <kate.yagu@gmail.com>
2023-01-31 14:14:01 +0300

    add two column to AboutCompany

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -5,7 +5,7 @@ function AboutCompany() {
     return (
         <div>
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex flex-column mt-5'>
+            <div className='row gap-5 mt-5 justify-content-around'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <Addresses/> {/* Список доступных адресов заведений */}
             </div>
@@ -13,4 +13,4 @@ function AboutCompany() {
     );
 }
 
-export default AboutCompany;
+export default AboutCompany; 
