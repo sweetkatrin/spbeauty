2263f51305ff989d2504c5c445d5c2d19ed79c7b
SweetKatrin <kate.yagu@gmail.com>
2023-01-31 14:15:20 +0300

    update style for list element and change SecondaryButton to IconOnlyButtonIconOnlyButton

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -4,20 +4,13 @@ import EditIcon from '../../../../../Icons/EditIcon';
 import RemoveIcon from '../../../../../Icons/RemoveIcon';
 import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 
-const Option = styled.div`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
-`;
 
 function ListElement(props) {
     return (
-        <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton>Ред.</SecondaryButton>
-            <SecondaryButton>Удл.</SecondaryButton>
+        <div className='d-flex flex-row align-items-center align-self-stretch gap-2 py-3'>
+            <div className='flex-grow-1 p-2'>{props.children}</div>
+            <IconOnlyButton icon={<EditIcon/>}/>
+            <IconOnlyButton icon={<RemoveIcon/>}/>
         </div>
     );
 }
