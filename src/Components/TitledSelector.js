80022be55b793cf0c76b9f5254e269ed850bda00
SweetKatrin <kate.yagu@gmail.com>
2023-01-31 14:08:49 +0300

    update style for TitledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 const Select = styled.select`
     background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
     background-position: calc(100% - 0.75rem) center !important;
+    background-color: #F8FAFC;
     -moz-appearance:none !important;
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
     border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border: 1px solid #E2E8F0;
+    border-radius: 24px;
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
@@ -19,11 +20,11 @@ const Select = styled.select`
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption'>{title}</div>);
     return (
-        <div className='d-flex flex-column gap-2'>
+        <div className='d-flex flex-column gap-1'>
             {Title}
-            <Select className='p-2'>
+            <Select className='px-3 py-2'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
